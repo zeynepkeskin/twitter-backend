b8b2fb4146e45ac81079ce6a3c48800b7ab33652
Bulent Keskin <bulentkeskin@gmail.com>
2023-06-13 01:54:32 -0700

    refactored app.js

--- src/app.js
+++ src/app.js
@@ -5,25 +5,28 @@ const authenticateUser = require("./middlewares/auth");
 const userController = require("./controllers/userController");
 const tweetController = require("./controllers/tweetController");
 
-const app = express();
-
-app.use(errorHandler);
-app.use(cors());
-app.use(express.json());
-
-const userRoutes = express.Router();
-userRoutes.post("/register", userController.register);
-userRoutes.post("/login", userController.login);
-userRoutes.get("/profile", authenticateUser, userController.getProfile);
-userRoutes.get("/", authenticateUser, userController.getUsers);
-app.use("/api/users", userRoutes);
-
-const tweetRoutes = express.Router();
-tweetRoutes.post("/", authenticateUser, tweetController.createTweet);
-tweetRoutes.get("/", tweetController.getTweetsForAnonymous);
-app.use("/api/tweets", tweetRoutes);
-
 const port = 3005;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+
+express()
+  .use(errorHandler)
+  .use(cors())
+  .use(express.json())
+  .use(
+    "/api/users",
+    express
+      .Router()
+      .get("/", authenticateUser, userController.getUsers)
+      .post("/", userController.register)
+      .post("/login", userController.login)
+      .get("/profile", authenticateUser, userController.getProfile)
+  )
+  .use(
+    "/api/tweets",
+    express
+      .Router()
+      .get("/", tweetController.getTweetsForAnonymous)
+      .post("/", authenticateUser, tweetController.createTweet)
+  )
+  .listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
